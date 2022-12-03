4a7f1a51b872d0df7d2f88f7d427dc5b6c0b3b43
Rahul Vishwakarma <vishwakarma.rahul77@gmail.com>
2022-12-03 18:27:51 +0530

    added register route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
-const {check,validationResult} = require('express-validator');
+const {check,validationResult, body} = require('express-validator');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 
@@ -11,18 +11,41 @@ const User = require('./../models/User');
 const token_key = process.env.TOKEN_KEY;
 
 
-//middleware setup
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
-//default route
-//public route
-//local localhost/500/api/users
+//Routes
+//api/users
 router.get('/',(req,res) => {
     res.status(200).json({
         status: 'success',
         message : 'user default route'
     })
 })
+
+router.post('/register',
+    [
+        //check empty fields
+        check('username').not().isEmpty().trim().escape(),
+        check('password').not().isEmpty().trim().escape(),
+        //check email
+        check('email').isEmail().normalizeEmail()
+    ],
+    (req,res)=>{
+        const errors = validationResult(req);
+        //check error is not empty
+        if(!errors.isEmpty()){
+            return res.status(404).json({
+                status: 'validate error',
+                errors: errors.array()
+            })
+        }
+
+        return res.status(200).json({
+            status : 'success',
+            data : req.body
+        })
+    }
+)
 
 module.exports = router;
